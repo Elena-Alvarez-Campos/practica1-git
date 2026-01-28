0cc9a4944298474a681392a0646c947611dd13ca
Elena-Alvarez-Campos <elenaalvarezcampos8@gmail.com>
2026-01-28 18:21:51 +0100

    creación del programa base

---
+++ practica1-git.js
@@ -0,0 +1,15 @@
+console.log("Ejemplo práctica de git")
+let cine={
+    nombre:"Cine-mira-mucho",
+    productos:[
+        {
+            nombre:"palomitas",
+            precio:5
+        },
+        {
+            nombre:"refresco",
+            precio:3
+        }
+    ]
+}
+//Crea una función para comprar palomitas y refrescos
